8ee873bb54295c12521b7744a3ab372e1d429e5e
yxfxcj <595305427@qq.com>
2025-03-20 16:05:16 +0800

    modify shop service

--- Services/IShopService.cs
+++ Services/IShopService.cs
@@ -5,6 +5,11 @@ namespace Ivan.DianPing.V2.Services
 {
     public interface IShopService
     {
+        /// <summary>
+        /// /sdf dsfsdf
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<Shop> GetShopByIdAsync(long id);
     }
 }
